84b89e154c870f3e22e8f980ea9a7bd7e6cb2bab
abeaufays <beaufays.alexandre@gmail.com>
2025-01-04 22:32:30 +0100

    Simplify run for answer in vscode

--- get_day_answer.py
+++ get_day_answer.py
@@ -1,9 +1,8 @@
 import importlib
 import sys
 
-year, day, part = sys.argv[1:4]
-if year.isnumeric() and day.isnumeric() and part in ("1", "2"):
-    day_module = importlib.import_module(f"y{year}.d{day}.part{part}")
-    print(day_module.answer(f"y{year}/d{day}/data"))
-else:
-    print("Expect three args: year day part (i.e. 24 02 1)")
+module = sys.argv[1]
+folder = sys.argv[2]
+
+day_module = importlib.import_module(module)
+print(day_module.answer(f"{folder}/data"))
