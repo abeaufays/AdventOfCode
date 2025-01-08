bf4072f86a4a7c5e0c9b726c59a248a38c31e315
abeaufays <beaufays.alexandre@gmail.com>
2025-01-08 18:36:57 +0100

    Add time measurement to get_day_answer

--- get_day_answer.py
+++ get_day_answer.py
@@ -1,8 +1,14 @@
 import importlib
 import sys
+import time
 
 module = sys.argv[1]
 folder = sys.argv[2]
 
 day_module = importlib.import_module(module)
+
+start = time.time()
 print(day_module.answer(f"{folder}/data"))
+end = time.time()
+
+print("Executed in", end - start, "seconds.")
