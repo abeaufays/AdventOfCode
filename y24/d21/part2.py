1f5b6786d15dc09aff20b748583b325eaf8cfde1
abeaufays <beaufays.alexandre@gmail.com>
2025-01-08 18:38:11 +0100

    WIP

---
+++ y24/d21/part2.py
@@ -0,0 +1,104 @@
+import functools
+from itertools import product
+from utils import maps
+
+
+CODE_LAYOUT = maps.str_to_map(
+    """
+789
+456
+123
+X0A""".lstrip()
+)
+
+INSTRUCTIONS_LAYOUT = maps.str_to_map(
+    """
+X^A
+<v>""".lstrip()
+)
+
+CODE_LAYOUT_ID = "CODE"
+INSTRUCTIONS_LAYOUT_ID = "INSTRUCTION"
+
+LAYOUTS = {CODE_LAYOUT_ID: CODE_LAYOUT, INSTRUCTIONS_LAYOUT_ID: INSTRUCTIONS_LAYOUT}
+
+
+@functools.cache
+def get_possible_instructions(from_: str, to_: str, layout_id: str) -> set[str]:
+    layout = LAYOUTS[layout_id]
+    from_position = maps.get_position_of(layout, from_)
+    to_position = maps.get_position_of(layout, to_)
+    x_position = maps.get_position_of(layout, "X")
+
+    up_number = from_position[0] - to_position[0]
+    left_number = from_position[1] - to_position[1]
+
+    horizontal_part = ("<" if left_number > 0 else ">") * abs(left_number)
+    vertical_part = ("^" if up_number > 0 else "v") * abs(up_number)
+    result = set()
+
+    if not (
+        (same_column := from_position[1] == x_position[1])
+        and (
+            will_cross := to_position[0] >= x_position[0]
+            if up_number >= 0
+            else to_position[0] <= x_position[0]
+        )
+    ):
+        result |= {vertical_part + horizontal_part + "A"}
+
+    if not (
+        (same_line := from_position[0] == x_position[0])
+        and (
+            will_cross := to_position[1] <= x_position[1]
+            if left_number >= 0
+            else to_position[1] >= x_position[1]
+        )
+    ):
+        result |= {horizontal_part + vertical_part + "A"}
+
+    return result
+
+
+@functools.cache
+def code_to_instructions(code: str, layout_id: str) -> set[str]:
+    start = "A"
+    results = {""}
+    for target in code:
+        new_instructions = get_possible_instructions(start, target, layout_id)
+        results = set(
+            "".join(instructions) for instructions in product(results, new_instructions)
+        )
+        start = target
+
+    return results
+    # min_len = len(min(results, key=len))
+    # return {result for result in results if len(result) == min_len}
+
+
+def parse_file(filename: str) -> list[str]:
+    with open(filename) as file:
+        return file.read().splitlines()
+
+
+def enter_code(code: str) -> str:
+    current_instructions = code_to_instructions(code, CODE_LAYOUT_ID)
+
+    for _ in range(3):
+        buffer: set[str] = set()
+        for instruction in current_instructions:
+            buffer |= code_to_instructions(instruction, INSTRUCTIONS_LAYOUT_ID)
+            min_len = min([len(x) for x in buffer])
+            buffer = {x for x in buffer if len(x) == min_len}
+        current_instructions = buffer
+    return min(current_instructions, key=len)
+
+
+def answer(filename: str) -> int:
+    codes = parse_file(filename)
+    result = 0
+    for code in codes:
+        numeric_part = int(code[:-1])
+        shortest_sequence_size = len(enter_code(code))
+        result += numeric_part * shortest_sequence_size
+    return result
---
+++ y24/d21/test_part1.py
@@ -0,0 +1,81 @@
+import pytest
+import y24.d21.part1 as solution
+
+
+def test_answer():
+    assert solution.answer("y24/d21/test") == 126384
+
+
+def test_parse_file():
+    assert solution.parse_file("y24/d21/test") == [
+        "029A",
+        "980A",
+        "179A",
+        "456A",
+        "379A",
+    ]
+
+
+@pytest.mark.parametrize(
+    ("code", "layout", "expected"),
+    [
+        ("029A", solution.CODE_LAYOUT_ID, "<A^A^^>AvvvA"),
+        ("379A", solution.CODE_LAYOUT_ID, "^A^^<<A>>AvvvA"),
+        (
+            "<A^A^^>AvvvA",
+            solution.INSTRUCTIONS_LAYOUT_ID,
+            "v<<A^>>A<A>A<AAv>A^Av<AAA^>A",
+        ),
+    ],
+)
+def test_code_to_instructions(code, layout, expected):
+    assert len(min(solution.code_to_instructions(code, layout), key=len)) == len(
+        expected
+    )
+
+
+@pytest.mark.parametrize(
+    ("code", "expected"),
+    [
+        (
+            "029A",
+            "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
+        ),
+        ("980A", "<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A"),
+        (
+            "179A",
+            "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
+        ),
+        ("456A", "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A"),
+        ("379A", "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A"),
+    ],
+)
+def test_enter_code(code, expected):
+    assert len(solution.enter_code(code)) == len(expected)
+
+
+def test_get_possible_instructions_doesnt_cross_x():
+    assert "^>>A" not in solution.get_possible_instructions(
+        "<", "A", solution.INSTRUCTIONS_LAYOUT_ID
+    )
+    assert "<vA" not in solution.get_possible_instructions(
+        "^", "<", solution.INSTRUCTIONS_LAYOUT_ID
+    )
+    assert "v>>A" not in solution.get_possible_instructions(
+        "1", "A", solution.CODE_LAYOUT_ID
+    )
+    assert "<<^^A" not in solution.get_possible_instructions(
+        "A", "4", solution.CODE_LAYOUT_ID
+    )
+
+    assert "<A" in solution.get_possible_instructions("A", "0", solution.CODE_LAYOUT_ID)
+    assert "vA" in solution.get_possible_instructions("2", "0", solution.CODE_LAYOUT_ID)
+    assert "vvA" in solution.get_possible_instructions(
+        "7", "1", solution.CODE_LAYOUT_ID
+    )
+    assert "^^A" in solution.get_possible_instructions(
+        "1", "7", solution.CODE_LAYOUT_ID
+    )
+    assert ">>vA" in solution.get_possible_instructions(
+        "1", "A", solution.CODE_LAYOUT_ID
+    )
