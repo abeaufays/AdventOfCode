7230a19a0158eb5f13b2236c5afec47e2ffecf76
Alexandre Beaufays <alexandre.beaufays@kraken.tech>
2025-10-12 16:17:25 +0200

    Apply ruff formating to bootstrap_day created files

--- bootstrap_day.sh
+++ bootstrap_day.sh
@@ -7,9 +7,10 @@ mkdir -p -- y"$1"/d"$2"
 touch y"$1"/__init__.py
 touch y"$1"/d"$2"/__init__.py
 
-echo "def answer(filename:str) -> int:
+echo "def answer(filename: str) -> int:
     return 0" > y"$1"/d"$2"/part1.py
 echo "import y"$1".d"$2".part1 as solution
+
 
 def test_answer():
     assert solution.answer(\"y"$1"/d"$2"/test\") == -1 # TO FILL" > y"$1"/d"$2"/test_part1.py
